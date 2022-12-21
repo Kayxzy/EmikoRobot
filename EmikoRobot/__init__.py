7fd63e2a907bfaf8de49982bb718a07ce1e0866b
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2022-12-21 13:13:16 +0700

    Update __init__.py

--- EmikoRobot/__init__.py
+++ EmikoRobot/__init__.py
@@ -7,7 +7,6 @@ import asyncio
 import time
 
 from inspect import getfullargspec
-from telethon.sessions import MemorySession
 from pyrogram.types import Message
 from pyrogram import Client, errors
 from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid, ChannelInvalid
